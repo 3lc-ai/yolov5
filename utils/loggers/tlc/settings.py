3ff9e42b1a0660641df24c0c362d29ca8be2844b
Frederik Mellbye <frederik.mellbye@3lc.ai>
2024-03-20 11:54:22 +0100

    Use set operations

--- utils/loggers/tlc/settings.py
+++ utils/loggers/tlc/settings.py
@@ -177,12 +177,12 @@ class Settings:
 
         Appropriate warnings are logged when unsupported environment variables are encountered.
         """
-        supported_env_vars = [self._field_to_env_var(_field) for _field in fields(Settings)]
-        unsupported_env_vars = [var for var in os.environ if var.startswith("TLC_") and var not in supported_env_vars]
+        supported_env_vars = set(self._field_to_env_var(_field) for _field in fields(Settings))
+        unsupported_env_vars = set(var for var in os.environ if var.startswith("TLC_")) - supported_env_vars
 
         # Do not warn about `tlcconfig` environment variables, as they are not part of the integration settings
         tlc_env_vars = set(option.envvar for option in options.OPTION.__subclasses__() if option.envvar)
-        unsupported_env_vars = [var for var in unsupported_env_vars if var not in tlc_env_vars]
+        unsupported_env_vars = unsupported_env_vars - tlc_env_vars
 
         # Output all environment variables if there are any unsupported ones
         if len(unsupported_env_vars) > 1:
