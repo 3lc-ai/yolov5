93d35813cf5e8e79f2f4462de47173e7c9100bc6
Frederik J. Mellbye <20386018+fredmell@users.noreply.github.com>
2024-04-26 17:40:11 +0200

    Warn instead of raising on incompatible `3lc` version (#38)

--- utils/loggers/tlc/version.py
+++ utils/loggers/tlc/version.py
@@ -4,20 +4,19 @@
 import tlc
 from packaging import version
 
+from utils.general import LOGGER, colorstr
 from utils.loggers.tlc.constants import TLC_VERSION_REQUIRED
-
 
 def check_tlc_version() -> None:
     """
     Check that the available 3LC version is supported.
 
-    :raises: ValueError if the 3LC version is not supported.
     """
     required_version = version.parse(TLC_VERSION_REQUIRED)
     installed_version = version.parse(tlc.__version__)
 
     if installed_version < required_version:
-        raise ValueError(
-            f"tlc version {required_version} or higher is required, but version {installed_version} is installed."
-            " Upgrade tlc or use an older version of the YOLOv5 integration."
-        )
+        prefix = colorstr("red", "WARNING: ")
+        LOGGER.warn(f"{prefix}You are using 3lc version {installed_version}, "
+                    f"but the integration is intended for {required_version} or higher. "
+                    "Please upgrade 3lc if you run into problems.")
